d476ebc84eadb8edd9d466b987372d4c75ffd53b
owfdataboy <phanvigiaii@gmail.com>
2021-12-23 09:53:23 +0700

    change file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,51 @@ import numpy as np
 from PIL import Image
 import streamlit as st
 import tensorflow as tf
-from helper import get_model
-from paddleocr import draw_ocr
+from paddleocr import draw_ocr, PaddleOCR
+
 
 st.markdown("<h1 style='text-align: center; color: black;'>OCR</h1>", unsafe_allow_html=True)
+
+def get_model(lang):
+    if lang == 'English':
+        ocr = PaddleOCR(use_angle_cls=True, lang='en')
+        return ocr, 'simfang.ttf'
+    elif lang == 'Arabic':
+        ocr = PaddleOCR(use_angle_cls=True, lang='ar')
+        return ocr, 'arabic.ttf'
+    elif lang == 'Welsh':
+        ocr = PaddleOCR(use_angle_cls=True, lang='cy')
+        return ocr, 'cyrillic.ttf'
+    elif lang == 'Hindi':
+        ocr = PaddleOCR(use_angle_cls=True, lang='hi')
+        return ocr, 'hindi.ttf'
+    elif lang == 'Nepali':
+        ocr = PaddleOCR(use_angle_cls=True, lang='ne')
+        return ocr, 'nepali.ttf'
+    elif lang == 'Spanish':
+        ocr = PaddleOCR(use_angle_cls=True, lang='es')
+        return ocr, 'spanish.ttf'
+    elif lang == 'Tamil':
+        ocr = PaddleOCR(use_angle_cls=True, lang='ta')
+        return ocr, 'tamil.ttf'
+    elif lang == 'Chinese':
+        ocr = PaddleOCR(use_angle_cls=True, lang='ch')
+        return ocr, 'simfang.ttf'
+    elif lang == 'Japan':
+        ocr = PaddleOCR(use_angle_cls=True, lang='japan')
+        return ocr, 'japan.ttc'
+    elif lang == 'Korean':
+        ocr = PaddleOCR(use_angle_cls=True, lang='korean')
+        return ocr, 'korean.ttf'
+    elif lang == 'French':
+        ocr = PaddleOCR(use_angle_cls=True, lang='fr')
+        return ocr, 'french.ttf'
+    elif lang == 'German':
+        ocr = PaddleOCR(use_angle_cls=True, lang='german')
+        return ocr, 'german.ttf'
+    elif lang == 'Italian':
+        ocr = PaddleOCR(use_angle_cls=True, lang='it')
+        return ocr, 'latin.ttf'
 
 def select_language():
     option = st.selectbox(
